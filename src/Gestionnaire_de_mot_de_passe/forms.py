c00585b0a7724e3f8b29701b393268ad5b33130e
Olivier Evrard <olivier.evrard29@gmail.com>
2023-07-01 20:32:39 +0200

    début d'ajout des filtre du MDP dans forms

--- src/Gestionnaire_de_mot_de_passe/forms.py
+++ src/Gestionnaire_de_mot_de_passe/forms.py
@@ -6,3 +6,28 @@ class SignupForm(forms.Form):
     last_name = forms.CharField(max_length=50)
     first_name = forms.CharField(max_length=50)
     birthdate = forms.DateField(input_formats=['%d/%m/%Y'])
+
+
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+        
+        
+    if len(password) < 12:
+        raise forms.ValidationError("Le mot de passe doit faire au moins 12 caractères")
+    else:
+        right_symbol_search = ['o', 'u']
+        bad_symbol_search = ['y', 'd']
+        count_symbol = 0
+        count_number = 0
+        count_uppercase = 0
+        count_lowercase = 0
+        for _ in password:
+            if _ in right_symbol_search:
+                i += 1
+            elif _ in bad_symbol_search:
+                raise forms.ValidationError("votre mot de passe contient un ou plusieurs symbole non autorisé(s)")
+            break
+
+        # if len(password) < 12:
+        #     raise forms.ValidationError("Le mot de passe doit faire au moins 12 caractères")
+        
---
+++ src/Gestionnaire_de_mot_de_passe/testnul.py
@@ -0,0 +1,35 @@
+# chaine = "UVGGUV"
+
+# lettres = " ".join(list(chaine))
+# lettres_virgules = lettres.replace(" ", ", ")
+
+# liste_mots = lettres_virgules.split(',')
+# # finded = []
+# # for element in liste_mots:
+# #     match element:
+# #         case "V" | "U" | "G":
+# #             finded += element
+        
+    
+# # print(finded)
+# # print(liste_mots)
+
+
+# finded = []
+
+
+# for i in liste_mots:
+#     match i:
+#         case "V" | "U" | "G":
+#             finded += i            
+#             liste_mots = liste_mots.remove(i)
+#     print(finded)
+
+chaine = "zrfzeozdzcffzuzfzeu"
+lettres_recherchees = ['o', 'u']
+i = 0
+
+for caractere in chaine:
+    if caractere in lettres_recherchees:
+        i += 1
+print(i)
