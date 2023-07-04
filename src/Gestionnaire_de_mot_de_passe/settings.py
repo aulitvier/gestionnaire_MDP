bc5684f9f7f81a028941f16529e8707e8cb1305a
Olivier EVRARD <olivier.evrard29@gmail.com>
2023-07-04 22:01:10 +0200

    installation postgresql

--- src/Gestionnaire_de_mot_de_passe/settings.py
+++ src/Gestionnaire_de_mot_de_passe/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Gestionnaire_de_mot_de_passe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gestionnaire_MDP',
+        'USER': 'olivi',
+        'PASSWORD': 'olivi852',
+        'HOST': 'localhost',
+        'PORT': '5432',
         
     }
 }
